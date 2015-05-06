67a6d4bed1dccf196a592605e41351b59f4194cb
Mattias Nilsen <kerp@chalmers.it>
2015-05-06 08:51:49 +0200

    sadfasfasfsfsafsdf

--- src/Cord.java
+++ src/Cord.java
@@ -6,8 +6,8 @@ public class Cord {
     public double x, y;
 
     public Cord() {
-        x = 0;
-        y = 0;
+        x = 0; // == r
+        y = 0; // == v
     }
 
     public Cord(double x, double y) {
@@ -16,17 +16,34 @@ public class Cord {
     }
 
     public void setPolar(double v ,double r) {
-        Cord c = PolarToRect(v, r);
+        Cord c = polarToRect(v, r);
         this.x = c.x;
         this.y = c.y;
     }
 
 
-    public Cord PolarToRect(double v, double r) {
+    public Cord polarToRect(double v, double r) {
         return new Cord(Math.cos(v) * r, Math.sin(v) * r);
     }
 
-    public Cord RectToPolar(double x, double y) {
+    public Cord polarToRect() {
+        return new Cord(Math.cos(y) * x, Math.sin(y) * x);
+    }
+
+    public Cord rectToPolar(double x, double y) {
         return new Cord(Math.sqrt(x * x + y * y), Math.atan2(y, x));
+    }
+
+    public Cord rectToPolar() {
+        return new Cord(Math.sqrt(x * x + y * y), Math.atan2(y, x));
+    }
+
+    public Cord sub(Cord other) {
+        return new Cord(this.x * Math.cos(this.y) - other.x * Math.cos(other.y), this.x * Math.sin(this.y) - other.x * Math.sin(other.y));
+    }
+
+    public Cord project(double vx, double vy) {
+        double div = (vx * x + vy * y) / (x * x + y * y);
+        return new Cord(div*x, div*y);
     }
 }
--- src/DummyModel.java
+++ src/DummyModel.java
@@ -17,27 +17,143 @@ public class DummyModel implements IBouncingBallsModel {
         this.areaWidth = width;
         this.areaHeight = height;
 
-        Balls.add(new Ball(areaWidth, areaHeight, 1, 4, 0, 10, 0.5));
-        Balls.add(new Ball(areaWidth, areaHeight, 2, 2, 2, 2, 0.5));
+        //Balls.add(new Ball(areaWidth, areaHeight, 3, 2, 2, 2, 0.5));
+        //Balls.add(new Ball(areaWidth, areaHeight, 8, 2, -2, 2, 0.5));
+
+        Balls.add(new Ball(areaWidth, areaHeight, 1, 4, 2, 2, 0.3));
+        Balls.add(new Ball(areaWidth, areaHeight, 2, 2, -2, 2, 0.3));
 
     }
 
     @Override
     public void tick(double deltaT) {
         
-    	for(Ball b : Balls) {
+    	/*for(Ball b : Balls) {
             b.tick(deltaT);
             for(Ball c : Balls){
             	if(b != c){
             		if(Math.pow((b.r-c.r), 2) <= Math.pow(b.x-c.x, 2) + Math.pow(b.y-c.y, 2) && Math.pow(b.x-c.x, 2) + Math.pow(b.y-c.y, 2) <= Math.pow((b.r+c.r), 2)){
-            			
+            			collide(b, c);
             		}
             	}
             }
+    }*/
+
+        Ball b = Balls.get(0);
+        Ball c = Balls.get(1);
+        if(Math.pow((b.r - c.r), 2) <= Math.pow(b.x - c.x, 2) + Math.pow(b.y - c.y, 2) && Math.pow(b.x - c.x, 2) + Math.pow(b.y - c.y, 2) <= Math.pow((b.r + c.r), 2)) {
+            collide(b, c, deltaT);
+        } else {
+            for (Ball a : Balls) {
+                a.tick(deltaT);
+            }
         }
     }
-    
-    
+
+    private void collide(Ball one, Ball two, double deltaT) {
+
+        Cord vOne = new Cord(one.vx, one.vy).rectToPolar();
+        Cord vTwo = new Cord(two.vx, two.vy).rectToPolar();
+
+        double a;
+        if(one.x - two.x == 0) {
+            a = Math.PI / 2;
+        } else {
+             a = Math.atan((one.y - two.y) / (one.x - two.x));
+        }
+
+        if ((one.y - two.y) / (one.x - two.x) >= 0) {
+            vOne.y -= a;
+            vTwo.y -= a;
+        } else {
+            vOne.y -= (Math.PI - a);
+            vTwo.y -= (Math.PI - a);
+         }
+
+        vOne = vOne.polarToRect();
+        vTwo = vTwo.polarToRect();
+
+        double u1 = vOne.x;
+        double u2 = vTwo.x;
+
+        double I = u1 + u2;
+        double R = -(u2 - u1);
+
+        double v1 = (I + 1*R / (1 + 1)) - R;
+        double v2 = (I + 1*R) / (1 + 1);
+
+        vOne.x = v1;
+        vTwo.x = v2;
+
+        vOne = vOne.rectToPolar();
+        vTwo = vTwo.rectToPolar();
+
+
+        if ((one.y - two.y) / (one.x - two.x) >= 0) {
+
+            vOne.y += a;
+            vTwo.y += a;
+            System.out.println("nej");
+        } else {
+            System.out.println("här");
+            vOne.y += (Math.PI - a);
+            vTwo.y += (Math.PI - a);
+        }
+
+        vOne = vOne.polarToRect();
+        vTwo = vTwo.polarToRect();
+
+        one.vx = vOne.x;
+        one.vy = vOne.y;
+
+        two.vx = vTwo.x;
+        two.vy = vTwo.y;
+
+        one.x += one.vx * deltaT;
+        one.y += one.vy * deltaT;
+
+        two.x += two.vx * deltaT;
+        two.y += two.vy * deltaT;
+        /*Cord oneC = new Cord(one.x, one.y).rectToPolar();
+        Cord twoC = new Cord(two.x, two.y).rectToPolar();
+
+        Cord v3one = oneC.sub(twoC);
+        Cord v3two = twoC.sub(oneC);
+
+        Cord projone = v3one.project(one.vx, one.vy);
+        Cord projtwo = v3one.project(two.vx, two.vy);
+
+        Cord cu1 = new Cord(projone.x, projone.y).rectToPolar();
+        Cord cu2 = new Cord(projtwo.x, projtwo.y).rectToPolar();
+
+        double ad = Math.atan2(projone.y, projone.x);
+        double da = Math.atan2(projtwo.y, projtwo.x);
+
+        double u1 = cu1.x;
+        double u2 = cu2.x;
+
+
+
+
+        //double u1 = Math.sqrt(projone.x * projone.x + projone.y * projone.y);
+        // double u2 = Math.sqrt(projtwo.x * projtwo.x + projtwo.y * projtwo.y);
+
+        double I = u1 + u2;
+        double R = -(u2 - u1);
+
+        double v1 = (I + 1*R / (1 + 1)) - R;
+        double v2 = (I + 1*R) / (1 + 1);
+
+        // Ball 1
+        Cord vOne = new Cord(v1 * Math.cos(Math.atan2(projone.y, projone.x)), v1 * Math.sin(Math.atan2(projone.y, projone.x)));
+        one.vx = one.vx - projone.x + vOne.x;
+        one.vy = one.vy - projone.y + vOne.y;
+
+        //Ball 2
+        Cord vTwo = new Cord(v2 * Math.cos(Math.atan2(projtwo.y, projtwo.x)), v2 * Math.sin(Math.atan2(projtwo.y, projtwo.x)));
+        two.vx = two.vx - projtwo.x + vTwo.x;
+        two.vy = two.vy - projtwo.y + vTwo.y;*/
+    }
 
     @Override
     public List<Ellipse2D> getBalls() {
